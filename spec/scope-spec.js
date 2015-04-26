652a1f03efef2c0e2e1ea3b5e02c02c60e8c062f
Samy Dindane <samy@dindane.com>
2015-04-26 02:58:04 +0200

    Rename "env" to "scope" everywhere

--- spec/scope-spec.js
+++ spec/scope-spec.js
@@ -2,31 +2,31 @@ var Scope = require('../src/scope.js');
 
 describe('Scope', function () {
     it('stores and returns the variables', function () {
-        var env = new Scope();
-        env.setValue('a', 1);
-        expect(env.getValue('a')).toEqual(1);
-        expect(env.getValue('?')).toBeUndefined();
+        var scope = new Scope();
+        scope.setValue('a', 1);
+        expect(scope.getValue('a')).toEqual(1);
+        expect(scope.getValue('?')).toBeUndefined();
     });
 
     it('updates the parent\'s variables', function () {
         var parent = new Scope();
         parent.setValue('a', 1);
 
-        var env = new Scope(parent);
-        env.setValue('a', 2);
-        env.setValue('b', 3);
+        var scope = new Scope(parent);
+        scope.setValue('a', 2);
+        scope.setValue('b', 3);
 
-        expect(env.getValue('a')).toEqual(2);
-        expect(env.getValue('b')).toEqual(3);
+        expect(scope.getValue('a')).toEqual(2);
+        expect(scope.getValue('b')).toEqual(3);
         expect(parent.getValue('a')).toEqual(2);
         expect(parent.getValue('b')).toBeUndefined();
     });
 
     it('accepts an object containing variables', function () {
         var vars = { 'a': 1, 'b': 2 };
-        var env = new Scope(null, vars);
+        var scope = new Scope(null, vars);
 
-        expect(env.getValue('a')).toEqual(1);
-        expect(env.getValue('b')).toEqual(2);
+        expect(scope.getValue('a')).toEqual(1);
+        expect(scope.getValue('b')).toEqual(2);
     });
 });
--- src/interpreter.js
+++ src/interpreter.js
@@ -23,30 +23,30 @@ var evalFn = function (fnName, args, functions) {
     return eval({type: 'body', value: fnDecl.body}, functions, new Scope(null, fnArguments));
 };
 
-var eval = function (expression, functions, env) {
+var eval = function (expression, functions, scope) {
     switch (expression.type) {
         case 'body':
-            return evalBody(expression, functions, env);
+            return evalBody(expression, functions, scope);
         case 'fnCall':
-            return evalFnCall(expression, functions, env);
+            return evalFnCall(expression, functions, scope);
         case 'string':
             return evalString(expression, functions);
         case 'integer':
             return evalInteger(expression, functions);
         case 'binOp':
-            return evalBinOp(expression, functions, env);
+            return evalBinOp(expression, functions, scope);
         case 'varAssignment':
-            return evalVarAssignation(expression, functions, env);
+            return evalVarAssignation(expression, functions, scope);
         case 'varAccess':
-            return evalVarAccess(expression, functions, env);
+            return evalVarAccess(expression, functions, scope);
         case 'if':
-            return evalIf(expression, functions, env);
+            return evalIf(expression, functions, scope);
         case 'while':
-            return evalWhile(expression, functions, env);
+            return evalWhile(expression, functions, scope);
         case 'list':
-            return evalList(expression, functions, env);
+            return evalList(expression, functions, scope);
         case 'deconstruction':
-            return evalDeconstruction(expression, functions, env);
+            return evalDeconstruction(expression, functions, scope);
             break;
         default:
             console.error(("Unknown expression type: " + expression.type));
@@ -54,51 +54,51 @@ var eval = function (expression, functions, env) {
     }
 };
 
-function evalIf(if_, functions, env) {
-    var cond = eval(if_.cond, functions, env);
+function evalIf(if_, functions, scope) {
+    var cond = eval(if_.cond, functions, scope);
     if (cond == 0 || cond == false) {
-        return eval(if_.falsePart, functions, new Scope(env));
+        return eval(if_.falsePart, functions, new Scope(scope));
     } else {
-        return eval(if_.truePart, functions, new Scope(env));
+        return eval(if_.truePart, functions, new Scope(scope));
     }
 }
 
-function evalWhile(while_, functions, env) {
-    while (eval(while_.cond, functions, env) != false) {
-        eval({type: 'body', value: while_.body}, functions, new Scope(env));
+function evalWhile(while_, functions, scope) {
+    while (eval(while_.cond, functions, scope) != false) {
+        eval({type: 'body', value: while_.body}, functions, new Scope(scope));
     }
 
     return null;
 }
 
-function evalDeconstruction(decons, functions, env) {
-    var list = eval(decons.value, functions, env);
+function evalDeconstruction(decons, functions, scope) {
+    var list = eval(decons.value, functions, scope);
     if (!_.isArray(list)) {
         console.error("Cannot deconstruct a value which is not a list.")
         process.exit();
     }
 
-    env.setValue(decons.head, list[0]);
-    env.setValue(decons.tail, list.slice(1));
+    scope.setValue(decons.head, list[0]);
+    scope.setValue(decons.tail, list.slice(1));
 }
 
-function evalList(list, functions, env) {
+function evalList(list, functions, scope) {
     return list.value.map(function (expr) {
-        return eval(expr, functions, env);
+        return eval(expr, functions, scope);
     });
 }
 
-function evalBody(body, functions, env) {
+function evalBody(body, functions, scope) {
     var result;
     body.value.forEach(function (expression) {
-        result = eval(expression, functions, env);
+        result = eval(expression, functions, scope);
     });
     return result;
 }
 
-function evalFnCall(fnCall, functions, env) {
+function evalFnCall(fnCall, functions, scope) {
     var args = fnCall.args.map(function (arg) {
-        return eval(arg, functions, env);
+        return eval(arg, functions, scope);
     });
 
     return evalFn(fnCall.target, args, functions);
@@ -112,43 +112,43 @@ function evalInteger(expression, functions) {
     return expression.value;
 }
 
-function evalBinOp(expression, functions, env) {
+function evalBinOp(expression, functions, scope) {
     switch (expression.op) {
         case '+':
-            return eval(expression.left, functions, env) + eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) + eval(expression.right, functions, scope);
         case '-':
-            return eval(expression.left, functions, env) - eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) - eval(expression.right, functions, scope);
         case '*':
-            return eval(expression.left, functions, env) * eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) * eval(expression.right, functions, scope);
         case '/':
-            return eval(expression.left, functions, env) / eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) / eval(expression.right, functions, scope);
         case '>':
-            return eval(expression.left, functions, env) > eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) > eval(expression.right, functions, scope);
         case '<':
-            return eval(expression.left, functions, env) < eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) < eval(expression.right, functions, scope);
         case '==':
-            var left  = eval(expression.left, functions, env);
-            var right = eval(expression.right, functions, env);
+            var left  = eval(expression.left, functions, scope);
+            var right = eval(expression.right, functions, scope);
             if (_.isArray(left) && _.isArray(right))
                 return (_(left).difference(right) == 0);
             return left == right;
         case '!=':
-            return eval(expression.left, functions, env) != eval(expression.right, functions, env);
+            return eval(expression.left, functions, scope) != eval(expression.right, functions, scope);
         default:
             throw new Error("Unrecognized operation.");
     }
 }
 
-function evalVarAssignation(expression, functions, env) {
-    env.setValue(expression.varName, eval(expression.e, functions, env), true);
+function evalVarAssignation(expression, functions, scope) {
+    scope.setValue(expression.varName, eval(expression.e, functions, scope), true);
 }
 
-function evalVarAccess(expression, functions, env) {
-    if (env.getValue(expression.varName) == undefined) {
+function evalVarAccess(expression, functions, scope) {
+    if (scope.getValue(expression.varName) == undefined) {
         console.error("Variable '" + expression.varName + "' is undefined.");
         process.exit();
     }
-    return env.getValue(expression.varName);
+    return scope.getValue(expression.varName);
 }
 
 exports.eval = eval;
